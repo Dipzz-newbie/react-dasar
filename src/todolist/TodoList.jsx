eb7d92e2f0d3eae4d103b78eafb7374c093ce104
Dipzz-newbie <mnadhiftriyudo@gmail.com>
2025-09-22 17:41:51 +0700

    Update

--- src/todolist/TodoList.jsx
+++ src/todolist/TodoList.jsx
@@ -1,14 +1,28 @@
 import Todo from "./Todo";
 
 const TodoList = () => {
+    const data = [
+        {text: "Belajar React", isComplated: false},
+        {text: "Belajar JavaScript", isComplated: true},
+        {text: "Belajar HTML", isComplated: true},
+        {text: "Belajar CSS", isComplated: true},
+        {text: "Belajar Typescript", isComplated: false},
+    ]
+    
+    const todos = data.map((todo) => {
+        return <Todo {...todo}/>
+    })
+
     return (
         <>
             <ul>
-            <Todo text="Belajar React" isComplated={false} />
-            <Todo text="Belajar JavaScript" isComplated={true}/> {/*>isDeleted={true}*/} 
-            <Todo text="Belajar HTML" isComplated={true}/>
-            <Todo text="Belajar CSS" isComplated={true}/>
-            <Todo text="Belajar Typescript" isComplated={false}/>
+            {/* <Todo text="Belajar React" isComplated={false} /> */}
+            {/* <Todo text="Belajar JavaScript" isComplated={true}/> {/*>isDeleted={true}*/} 
+            {/* <Todo text="Belajar HTML" isComplated={true}/> */}
+            {/* <Todo text="Belajar CSS" isComplated={true}/> */}
+            {/* <Todo text="Belajar Typescript" isComplated={false}/> */}
+            {todos}
+
             </ul>
         </>
     )
