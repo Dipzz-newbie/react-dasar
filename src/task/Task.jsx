d5ab639ae38f017e7723ad92e1b5d874e19220e6
Dipzz-newbie <mnadhiftriyudo@gmail.com>
2025-09-23 18:10:33 +0700

    Update

--- src/task/Task.jsx
+++ src/task/Task.jsx
@@ -1,8 +1,34 @@
+import { useState } from "react";
+import { useImmer } from "use-immer";
+
+
 const Task = () => {
+    const [item, setItem] = useState("")
+    const [items, setItems] = useImmer([])
+
+    const handleChange = (e) => {
+        setItem(e.target.value)
+    }
+
+    const handleClick = (e) => {
+        e.preventDefault()
+        setItems((items) => void(items.push(item)))
+        setItem("")
+    }
+
     return(
-        <>
-        <h1>hello i'm in task page</h1>
-        </>
+        <div>
+            <h1>Create Task</h1>
+            <form action="">
+                <input type="text" placeholder="Task" value={item} onChange={handleChange} />
+                <button type="submit" onClick={handleClick}>Add</button>
+            </form>
+
+            <h1>List Task</h1>
+            <ul>
+                {items.map((item, index) => <li key={index}>{item}</li>)}
+            </ul>
+        </div>
     )
 }
 
