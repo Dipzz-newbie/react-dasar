4cca90d51ad25b585ec6a871b223346d00e906a9
Dipzz-newbie <mnadhiftriyudo@gmail.com>
2025-09-25 16:16:50 +0700

    Update

--- src/note/Note.jsx
+++ src/note/Note.jsx
@@ -12,9 +12,28 @@ const Note = ({note, onChange, onDelete}) => {
     if(isEditing) {
         component = (
             <>
-                <input type="text" placeholder="input" onChange={handleChangeText}/>
+                <input type="text" placeholder="input" onChange={handleChangeText} value={note.text}/>
                 <button onClick={() => setIsEditing(false)}>Save</button>
             </>
         )
+    } else {
+        component = (
+            <>
+                {note.text}
+                <button onClick={() => setIsEditing(true)}>Edit</button>
+            </>
+        )
     }
+
+    const handleCheckDone = (e) => {
+        const newNote = {...note, done:e.target.checked}
+        onChange(newNote)
+    }
+
+    return(
+        <>
+            <input type="checkbox" checked={note.done} onChange={handleCheckDone}/>
+            <button onClick={() => onDelete(note)}>Delete</button>
+        </> 
+    )
 }
