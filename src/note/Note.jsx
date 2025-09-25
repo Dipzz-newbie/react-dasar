e24f2bd7e444b38a75efa9ffea2d9f8491f2d43a
Dipzz-newbie <mnadhiftriyudo@gmail.com>
2025-09-25 16:07:57 +0700

    Update

--- src/note/Note.jsx
+++ src/note/Note.jsx
@@ -1,6 +1,20 @@
 import { useState } from "react"
 
 
-const Note = () => {
+const Note = ({note, onChange, onDelete}) => {
     const [isEditing, setIsEditing] = useState(false)
+    let component;
+    const handleChangeText = (e) => {
+        const newNote = {...note, text:e.target.value}
+        onChange(newNote)
+    }
+
+    if(isEditing) {
+        component = (
+            <>
+                <input type="text" placeholder="input" onChange={handleChangeText}/>
+                <button onClick={() => setIsEditing(false)}>Save</button>
+            </>
+        )
+    }
 }
