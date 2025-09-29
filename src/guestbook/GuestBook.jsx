d7e46d0dd697497db0f6dc8eb779e4c2925dec64
Dipzz-newbie <mnadhiftriyudo@gmail.com>
2025-09-29 19:28:01 +0700

    Update

--- src/guestbook/GuestBook.jsx
+++ src/guestbook/GuestBook.jsx
@@ -1,9 +1,39 @@
+import { useRef, useState } from "react";
 
 
 const GuestBook = () => {
+
+    const [name, setName] = useState("")
+    const [massage, setMassage] = useState("")
+
+    const nameInput = useRef(null)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setName("")
+        setMassage("")
+
+        nameInput.current.focus();
+
+        alert(`Name : ${name} Massage : ${massage}`)
+    }
+
     return(
         <>
-            <h1>hello i'm from GuestBook</h1>
+            <h1>GuestBook</h1>
+            <form>
+                <label htmlFor="name">Name</label> <br/>
+                <input ref={nameInput} type="text" name="name" value={name} onChange={(e) => {
+                    setName(e.target.value)
+                }} /> <br/>
+
+                <label htmlFor="massage">Massage</label> <br/>
+                <textarea name="massage" value={massage} onChange={(e) => {
+                    setMassage(e.target.value)
+                }}>Massage</textarea> <br/>
+                <button onClick={handleSubmit}>Submit</button>
+
+            </form>
         </>
     )
 }
