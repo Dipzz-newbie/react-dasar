fd6844979872e3c64026c5d7377879517d69170b
Dipzz-newbie <mnadhiftriyudo@gmail.com>
2025-09-30 19:10:03 +0700

    Update

--- src/product/ProductList.jsx
+++ src/product/ProductList.jsx
@@ -1,25 +1,30 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import Product from "./Product";
 
 
 const ProductList = () => {
 
     const [products, setProducts] = useState([])
-    const loaded = useRef(false)
+    const [loaded, setLoaded] = useState(false)
+
+    const handleClick = () => {
+        setLoaded(true)
+    }
 
     useEffect(() => {
-        if(loaded.current === false){
-            fetch("/product.json").then((response) => response.json()).then((data) => {setProducts(data)}).then(loaded.current = true)
+        if(loaded){
+            fetch("/product.json").then((response) => response.json()).then((data) => {setProducts(data)})
         }
 
         return () => {
             console.log("Product List component unmounted")
         }
-    })
+    }, [loaded])
 
     return(
         <div>
             <h1>List Product</h1>
+            <button onClick={handleClick}> Load Product</button>
             <p>{products.map((product) => (
                 <Product key={product.id} product={product}/>
             ))}</p>
