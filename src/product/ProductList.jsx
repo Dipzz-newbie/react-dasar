fc00c81c18bed4991ed075d67252a3c357594287
Dipzz-newbie <mnadhiftriyudo@gmail.com>
2025-09-30 18:49:26 +0700

    Update

--- src/product/ProductList.jsx
+++ src/product/ProductList.jsx
@@ -1,8 +1,25 @@
+import { useEffect, useRef, useState } from "react";
+import Product from "./Product";
 
 
 const ProductList = () => {
+
+    const [products, setProducts] = useState([])
+    const loaded = useRef(false)
+
+    useEffect(() => {
+        if(loaded.current === false){
+            fetch("/product.json").then((response) => response.json()).then((data) => {setProducts(data)}).then(loaded.current(true))
+        }
+    })
+
     return(
-        <>Hello from Product!</>
+        <div>
+            <h1>List Product</h1>
+            <p>{products.map((product) => (
+                <Product key={product.id} product={product}/>
+            ))}</p>
+        </div>
     )
 }
 
